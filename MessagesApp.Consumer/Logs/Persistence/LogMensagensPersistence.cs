ab70c67f36698de0cd638fba90b774d1232a1097
arthurvalentereis <arthur.valente.reis@gmail.com>
2023-07-19 23:53:23 -0300

    corrigindo appsettings

--- MessagesApp.Consumer/Logs/Persistence/LogMensagensPersistence.cs
+++ MessagesApp.Consumer/Logs/Persistence/LogMensagensPersistence.cs
@@ -21,6 +21,24 @@ namespace MessagesApp.Consumer.Logs.Persistence
 
         public List<LogMensagens> GetAll()
         {
+
+            var logMensagensCollection = _mongoDBContext.LogMensagens.Database.GetCollection<LogMensagens>("logmensagens");
+
+
+            var logDocumentList = logMensagensCollection.Find(Builders<LogMensagens>.Filter.Empty).ToList();
+
+            foreach (var logDocument in logDocumentList)
+            {
+                // Aqui você pode acessar as propriedades individuais do documento
+                var id = logDocument.Id;
+                var dataHoraEnvio = logDocument.DataHoraEnvio;
+                var emailPara = logDocument.EmailPara;
+                var assunto = logDocument.Assunto;
+                var conteudo = logDocument.Conteudo;
+
+                // Faça o que precisar com as informações do log
+            }
+
             var filter = Builders<LogMensagens>.Filter.Where(l => true);
             return _mongoDBContext.LogMensagens
                 .Find(filter)
